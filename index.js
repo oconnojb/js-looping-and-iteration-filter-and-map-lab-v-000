ea362fc55e09d8a7efbc852c32772af26a3dec0b
John O'Connor <oconnojb@gmail.com>
2018-07-28 20:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // Code your solution here:
-function driversWithRevenueOver(arry, n) {
-  return arry.filter( function (dr) {
+function driversWithRevenueOver(arr, n) {
+  return arr.filter( function (dr) {
     return dr['revenue'] > n;
+  })
+}
+
+function driverNamesWithRevenueOver(arr, n) {
+  let filtered = arr.filter( function (dr) {
+    return dr['revenue'] > n;
+  })
+
+  return filtered.map( function (dr) {
+    return dr['name'];
   })
 }
